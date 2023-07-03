64f319b5d62fc504adc9db32f0dc377670f46d49
mohamedjagne <mohamedjagne110@gmail.com>
2023-07-03 10:51:17 +0300

    hero navbar components added

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500;600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
