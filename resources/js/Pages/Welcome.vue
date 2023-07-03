c4d1199c7e67ad2544afcf04ddab95d0d7d3066c
mohamedjagne <mohamedjagne110@gmail.com>
2023-07-03 12:09:56 +0300

    categories section added to home page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,6 +3,7 @@
     import Navbar from '@/Components/Navbar.vue'
     import Hero from '@/Components/Hero.vue'
     import Card from '@/Components/Card.vue'
+    import CategoryCard from '@/Components/CategoryCard.vue'
 </script>
 
 <template>
@@ -16,6 +17,15 @@
             <Card />
             <Card />
             <Card />
+        </div>
+    </div>
+    <div class="max-w-6xl mx-auto mb-11 p-4">
+        <h1>Categories</h1>
+        <div class="mt-3 grid grid-rows-1 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2">
+            <CategoryCard />
+            <CategoryCard />
+            <CategoryCard />
+            <CategoryCard />
         </div>
     </div>
 </template>
